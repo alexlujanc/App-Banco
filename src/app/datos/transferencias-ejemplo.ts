64bc36b2de5aff9e4b65f2f86ce5297df088d48e
alexlujanc <alexlujanc96@gmail.com>
2023-06-14 13:16:07 +0200

    Actualizado

---
+++ src/app/datos/transferencias-ejemplo.ts
@@ -0,0 +1,28 @@
+export const transferenciasRecibidas = [
+    {
+        id: 1,
+        idOrdenante: 1,
+        idBeneficiario: 2,
+        importe: 1000,
+        concepto: "Viaje",
+        fecha: new Date(),
+
+    },
+    {
+        id: 2,
+        idOrdenante: 2,
+        idBeneficiario: 1,
+        importe: 500,
+        concepto: "Comida",
+        fecha: new Date()
+      },
+
+      {
+        id: 3,
+        idOrdenante: 2,
+        idBeneficiario: 1,
+        importe: 2000,
+        concepto: "Hospedaje",
+        fecha: new Date()
+      }
+]
