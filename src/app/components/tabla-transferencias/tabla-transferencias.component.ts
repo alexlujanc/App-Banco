a9b995c49d02902f8f574b5d5708f65fad111479
alexlujanc <alexlujanc96@gmail.com>
2023-06-14 14:16:21 +0200

    Cambios mas

---
+++ src/app/components/tabla-transferencias/tabla-transferencias.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-transferencias',
+  templateUrl: './tabla-transferencias.component.html',
+  styleUrls: ['./tabla-transferencias.component.css']
+})
+export class TablaTransferenciasComponent {
+
+}
